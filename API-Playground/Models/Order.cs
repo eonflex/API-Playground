6bf4a2b90d2938938a7eb89cdba4c49887eb8b9f
Jonathan Hay <jhay@marian.edu>
2026-01-13 10:04:39 -0500

    Added order file to models

---
+++ API-Playground/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace API_Playground.Models
+{
+    public sealed class Order
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
